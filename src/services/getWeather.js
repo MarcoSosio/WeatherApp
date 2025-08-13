ed64f196c35e876ad49489b37ad78cf4ad9a6919
Marco Sosio <marcososio007@gmail.com>
2025-08-14 01:20:17 +0200

    changed HTTP to HTTPS

--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -1,6 +1,6 @@
 export default function getWeather(cityParam) {
     const city = encodeURIComponent(cityParam.toLowerCase())
-    const url=`http://api.weatherapi.com/v1/current.json?key=9e8c058a3ebc4d20b0e203133250908&q=${city}&aqi=yes&lang=it`
+    const url=`https://api.weatherapi.com/v1/current.json?key=9e8c058a3ebc4d20b0e203133250908&q=${city}&aqi=yes&lang=it`
     return (
         fetch(url)
         .then(response => response.json())
